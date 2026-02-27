39402cc58f47e105bd40065cbe85e89d92ff1e3b
MK-EHE <kontakt.mateusz.kudas@gmail.com>
2026-02-27 03:00:35 +0100

    get_acords function

--- scraper.py
+++ scraper.py
@@ -7,10 +7,12 @@ class FragranticaScraper:
 
     def get_first_or_none(self, tree, xpath_query):
         result = tree.xpath(xpath_query)
-        if result:
-            return result[0].strip()
-        else:
-            return None
+        return result[0].strip() if result else None
+
+
+    def get_acords(self, tree, xpath_query):
+        accords = tree.xpath(xpath_query)
+        return {i: accord for i, accord in enumerate(accords, start=1) if accord.strip()}
 
 
     def fetch_page(self, url):
@@ -25,7 +27,8 @@ class FragranticaScraper:
         
         return response.content
     
-    def get_perfume_data(self, url):
+    
+    def get_basic_data(self, url):
         html_content = self.fetch_page(url)
         tree = html.fromstring(html_content)
 
@@ -34,13 +37,13 @@ class FragranticaScraper:
             "gender": self.get_first_or_none(tree, '//*[@id="toptop"]/h1/span/text()'),
             "rating": self.get_first_or_none(tree, '//*[@id="app"]/main/div/div[1]/div[1]/div[4]/div[3]/p/span[1]/text()'),
             "amount_of_rates": self.get_first_or_none(tree, '//*[@id="app"]/main/div/div[1]/div[1]/div[4]/div[3]/p/span[3]/text()'),
-            "acords": {acord: self.get_first_or_none(tree, f'//*[@id="app"]/main/div/div[1]/div[1]/div[2]/div[2]/div/div/div[{acord}]/div/span/text()') for acord in range(1, 11)},
+            "acords": self.get_acords(tree, '//*[@id="app"]/main/div/div[1]/div[1]/div[2]/div[2]/div/div//span/text()'),
             "url": url
         }
         return data
     
 
 scraper = FragranticaScraper()
-data = scraper.get_perfume_data("https://www.fragrantica.pl/perfumy/Guerlain/Guerlain-Homme-Eau-de-Parfum-2016--47793.html")
+data = scraper.get_basic_data("https://www.fragrantica.com/perfume/Fragrance-One/Office-For-Men-55166.html")
 
 print(data)
